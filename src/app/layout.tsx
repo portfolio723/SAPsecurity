d04a353904197d241f9e8d4624be814859140980
Sandeep M <723msfree@gmail.com>
2026-01-12 07:52:50 +0000

    also update the fav.png as the icon for the browser tab open by the user

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'SAP Security Expert Hub',
   description: 'Your central resource for SAP security insights and expertise.',
+  icons: {
+    icon: '/fav.png',
+  },
 };
 
 export default function RootLayout({
@@ -14,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/fav.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Source+Sans+Pro:wght@600;700&display=swap" rel="stylesheet" />
