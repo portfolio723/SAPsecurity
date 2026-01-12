2690c604683d33459b000a3a238827386f103d1f
Sandeep M <723msfree@gmail.com>
2026-01-12 07:04:05 +0000

    update the logo with public/we.png

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
     <Image
-      src="/logo.png"
+      src="/we.png"
       alt="SAP Security Expert Hub Logo"
       width={40}
       height={40}
