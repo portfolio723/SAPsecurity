878ae936192351a55b8963e80740c8bd0c980dcf
Sandeep M <723msfree@gmail.com>
2026-04-27 05:07:51 +0000

    Add the Next.js component Import and use the <SpeedInsights/> Next.js co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -22,7 +23,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Source+Sans+Pro:wght@600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">{children}</body>
+      <body className="font-body antialiased">
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
